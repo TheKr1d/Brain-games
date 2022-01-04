9dfb53a0c7cc7ff8f31fc6834d2280c95e4b2b41
TheKr1d <epifanov.vv.spb@gmail.com>
2022-01-04 13:03:08 +0300

    add pack eslint, eslint-config-airbnb-base, eslint-plugin-import, add make lint, setting lint

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
-export default function greeting () {
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
-};
+
+export default function greeting() {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+}
