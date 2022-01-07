33c16c2298e1454ba9f8eeca2c3931251b74ac97
TheKr1d <epifanov.vv.spb@gmail.com>
2022-01-08 00:36:59 +0300

    correcction ESlint

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,24 +1,9 @@
 #!/usr/bin/env node
-import engine from '../src/games/index.js';
-import _ from "lodash";
+import {
+  engine, getStringFromArrey, randomNumber, rendomOperator,
+} from '../src/games/index.js';
 
-const operators = ['+', '-', '*'];
 const condition = 'What is the result of the expression?';
-
-const getArr = (arr) => {
-  const arrString = String(arr).split(' ');
-  switch (arrString[1]) {
-    case '-':
-      return arrString[0] - arrString[2];
-    case '*':
-      return arrString[0] * arrString[2];
-    case '+':
-      return arrString[0] + arrString[2];
-    default:
-      break;
-  }
-  return null;
-};
 
 const getQuestionAndAnswer = () => {
   let i = 0;
@@ -26,8 +11,8 @@ const getQuestionAndAnswer = () => {
 
   while (i !== 3) {
     const arr = [];
-    arr.push(`${Math.floor(Math.random() * 1000)} ${operators[Math.floor(Math.random() * 3)]} ${Math.floor(Math.random() * 1000)}`);
-    arr.push(getArr(arr[0]));
+    arr.push(`${randomNumber()} ${rendomOperator()} ${randomNumber()}`);
+    arr.push(getStringFromArrey(arr[0]));
     resultArr[i] = arr;
     i += 1;
   }
--- src/games/index.js
+++ src/games/index.js
@@ -1,13 +1,15 @@
 import readlineSync from 'readline-sync';
 
-const engine = (condition, quest) => {
+const operators = ['+', '-', '*'];
+
+export const engine = (condition, quest) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   const name = userName;
   console.log(condition);
   let i = 0;
-  while (i <= 3) {
+  while (i !== 3) {
     const item = quest[i];
     console.log(`Question: ${item[0]}`);
     const answer = readlineSync.question('Your answer: ');
@@ -18,4 +20,21 @@ const engine = (condition, quest) => {
   }
   return console.log(`Congratulations, ${name}!`);
 };
-export default engine;
+
+export const getStringFromArrey = (arr) => {
+  const arrString = String(arr).split(' ');
+  switch (arrString[1]) {
+    case '-':
+      return Number(arrString[0]) - Number(arrString[2]);
+    case '*':
+      return Number(arrString[0]) * Number(arrString[2]);
+    case '+':
+      return Number(arrString[0]) + Number(arrString[2]);
+    default:
+      break;
+  }
+  return null;
+};
+
+export const randomNumber = () => Math.floor(Math.random() * 1000);
+export const rendomOperator = () => operators[Math.floor(Math.random() * (operators.length - 1))];
