81e2a70c5e6953d06c8b0114883585d6c3aa819f
TheKr1d <epifanov.vv.spb@gmail.com>
2022-01-07 19:53:49 +0300

    resset

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,20 +1,36 @@
 #!/usr/bin/env node
-import engine from '../src/games/index.js'
+import engine from '../src/games/index.js';
+import _ from "lodash";
 
-  const operators = ['+', '-', '*'];
-  const condition = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
+const condition = 'What is the result of the expression?';
 
-  const getQuestionAndAnswer = () => {
-      let i = 0;
-      const resultArr = [];
-
-      while (i !== 3) {
-          const arr = [];
-          arr.push(`${Math.floor(Math.random() * 1000)} ${operators[Math.floor(Math.random() * 3)]} ${Math.floor(Math.random() * 1000)}`);
-          arr.push(eval(arr[0]));
-          resultArr[i] = arr;
-          i += 1;
-      }
-      return resultArr;
+const getArr = (arr) => {
+  const arrString = String(arr).split(' ');
+  switch (arrString[1]) {
+    case '-':
+      return arrString[0] - arrString[2];
+    case '*':
+      return arrString[0] * arrString[2];
+    case '+':
+      return arrString[0] + arrString[2];
+    default:
+      break;
   }
+  return null;
+};
+
+const getQuestionAndAnswer = () => {
+  let i = 0;
+  const resultArr = [];
+
+  while (i !== 3) {
+    const arr = [];
+    arr.push(`${Math.floor(Math.random() * 1000)} ${operators[Math.floor(Math.random() * 3)]} ${Math.floor(Math.random() * 1000)}`);
+    arr.push(getArr(arr[0]));
+    resultArr[i] = arr;
+    i += 1;
+  }
+  return resultArr;
+};
 engine(condition, getQuestionAndAnswer());
--- src/games/index.js
+++ src/games/index.js
@@ -1,21 +1,21 @@
 import readlineSync from 'readline-sync';
 
 const engine = (condition, quest) => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-    let name = userName;
-    console.log(condition);
-    let i = 3;
-    let resultUser = 0;
-
-    for (const item of quest) {
-        console.log(`Question: ${item[0]}`);
-        const answer = readlineSync.question('Your answer: ');
-        if (answer !== String(item[1])) {
-            return console.log(`"${answer}" is wrong answer ;(. Correct answer was "${item[1]}"\nLet's try again, ${name}!`);
-        }
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  const name = userName;
+  console.log(condition);
+  let i = 0;
+  while (i <= 3) {
+    const item = quest[i];
+    console.log(`Question: ${item[0]}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer !== String(item[1])) {
+      return console.log(`"${answer}" is wrong answer ;(. Correct answer was "${item[1]}"\nLet's try again, ${name}!`);
     }
-    return console.log(`Congratulations, ${name}!`);
-}
+    i += 1;
+  }
+  return console.log(`Congratulations, ${name}!`);
+};
 export default engine;
